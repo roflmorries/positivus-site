e393a04cd265deced5b90cb70310ac6bac569b84
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-01-30 23:18:40 +0200

    added new features

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const themeToggle = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+}
+
+
+function toggleTheme() {
+    const isDark = document.documentElement.getAttribute('data-theme') === 'dark';
+    document.documentElement.setAttribute('data-theme', isDark ? 'light' : 'dark');
+    localStorage.setItem('theme', isDark ? 'light' : 'dark');
+    console.log('action')
+}
+
+themeToggle.addEventListener('click', toggleTheme)
